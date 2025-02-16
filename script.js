cdfd1fe32282e8200923ee2d9ae4f0c4dd1e6e30
Serhii Dydunets <sergijdudinec69@gmail.com>
2025-02-16 21:53:41 +0200

    addd

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// const x = Number(prompt("Введіть перше число:"));
+const x = Number(prompt("Введіть перше число:"));
 const y = Number(prompt("Введіть друге число:"));
 
 alert(x + " + " + y + " = " + (x + y));
