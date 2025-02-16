28f25dd89aa0c0342ff97f1d2f001d7d93922466
Serhii Dydunets <sergijdudinec69@gmail.com>
2025-02-16 21:49:55 +0200

    add

--- script.js
+++ script.js
@@ -1,33 +1,18 @@
-const ask = confirm("згода на опитування");
-if (ask) {
-    alert("дякую за згоду")
-    const ask2 = prompt("cvv code your card");
-    console.log("ask2");
-} else {
-    alert("опитування відмінено");
-}
-const question = confirm("повідомлення про небезпеку");
-console.log("question");
-if (question) {
-    alert("Дякую за підтвердження!");
-    const question2 = alert("ви успішно купили це товар!");
-    console.log("question2");
-} else {
-    alert("Дію відмінено!")
-}
-const install = confirm("встановити");
-console.log("install");
-if (install) {
-    alert("встановлення відбуваєтся")
-    const ask = confirm("цей файл може нашкодити вашому пк..");
-    console.log("ask");
-    if (ask) {
-        alert("загрозу усунено")
-        alert("програму встановлено")
-        alert("дякуюємо за довіру!!")
-    } else {
-        alert("дію відмінено")
-    }
-} else {
-    alert("дію відмінено:(")
-}  
+// const x = Number(prompt("Введіть перше число:"));
+const y = Number(prompt("Введіть друге число:"));
+
+alert(x + " + " + y + " = " + (x + y));
+
+const first = Number(prompt("first number"));
+const second = Number(prompt("second number"));
+alert(first + "-" + second + "=" + (first - second));
+
+const z = Number(prompt("first number"));
+const r = Number(prompt("second number"));
+
+alert(z + "*" + r + "=" + (z * r));
+
+const p = Number(prompt("first number"));
+const o = Number(prompt("second number"));
+
+alert(p + "/" + o + "=" + (p / o));
