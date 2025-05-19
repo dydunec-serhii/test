47783515452e0520f4a03b78a1b55d9bd6e4b721
Serhii Dydunets <sergijdudinec69@gmail.com>
2025-05-19 16:03:53 +0300

    фвв

--- script.js
+++ script.js
@@ -199,5 +199,97 @@
 //     console.log(i);
 //     i++
 // }
-const text1 = prompt('введіть ваш текст!');
-const text2 = prompt('введіть ваш текст!');
+// const text1 = prompt('введіть ваш текст!');
+// const text2 = prompt('введіть ваш текст!');
+// const num = [1, 2, 3, 4, 5];
+// const x = x => x > 2;
+// console.log(num.some(x));
+// console.log(num.find(x => x > 3));
+// console.log(num.filter(x => x > 3));
+// const players = [
+//     { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//     { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//     { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//     { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//     { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+// ];
+// console.log(players.find(elemet => players.id = 'player-1'))
+// console.log(players.find(elemen => players.name = 'Poly'));
+// console.log(players.every(element => players.timePlayed > 200));
+// console.log(players.every(elemen => players.online = true));
+// const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(num.filter(ele => ele % 2 === 0));
+// function nume(arr) {
+//     let x = 0;
+//     for (let i = 0; i < num.length; i++) {
+//         const para = Math.num % 2 === 0;
+
+//     }
+// }
+// const people = [
+//     { name: 'John', age: 32, occupation: 'programmer' },
+//     { name: 'Jane', age: 26, occupation: 'teacher' },
+//     { name: 'Mike', age: 42, occupation: 'engineer' },
+//     { name: 'Emily', age: 17, occupation: 'designer' }
+//   ];
+//   console.log(people.filter( ele => ele.age > 18).map(ele => ele.name));
+const users = [
+
+    {
+
+        id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081', name: 'Moore Hensley', email: 'moorehensley@indexia.com',
+        eyeColor: 'blue', friends: ['Sharron Pace'], isActive: false, balance: 2811, skills: ['ipsum', 'lorem'],
+        gender: 'male', age: 37,
+
+    },
+
+    {
+        id: '7a3cbd18-57a1-4534-8e12-1caad921bda1', name: 'Sharlene Bush', email: 'sharlenebush@tubesys.com',
+        eyeColor: 'blue', friends: ['Briana Decker', 'Sharron Pace'], isActive: true,
+        balance: 3821, skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'], gender: 'female', age: 34,
+
+    },
+
+    {
+        id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3', name: 'Ross Vazquez', email: 'rossvazquez@xinware.com',
+        eyeColor: 'green', friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'], isActive: false,
+        balance: 3793, skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'], gender: 'male', age: 24,
+
+    },
+
+    {
+        id: '249b6175-5c30-44c6-b154-f120923736f5', name: 'Elma Head', email: 'elmahead@omatom.com',
+        eyeColor: 'green', friends: ['Goldie Gentry', 'Aisha Tran'], isActive: true, balance: 2278,
+        skills: ['adipisicing', 'irure', 'velit'], gender: 'female', age: 21,
+
+    },
+
+    {
+
+        id: '334f8cb3-eb04-45e6-abf4-4935dd439b70', name: 'Carey Barr', email: 'careybarr@nurali.com',
+        eyeColor: 'blue', friends: ['Jordan Sampson', 'Eddie Strong'], isActive: true,
+        balance: 3951, skills: ['ex', 'culpa', 'nostrud'],
+        gender: 'male', age: 27,
+
+    },
+
+    {
+        id: '150b00fb-dd82-427d-9faf-2879ea87c695', name: 'Blackburn Dotson', email: 'blackburndotson@furnigeer.com',
+        eyeColor: 'brown', friends: ['Jacklyn Lucas', 'Linda Chapman'], isActive: false, balance: 1498,
+        skills: ['non', 'amet', 'ipsum'], gender: 'male', age: 38,
+
+    },
+
+    {
+        id: 'e1bf46ab-7168-491e-925e-f01e21394812', name: 'Sheree Anthony', email: 'shereeanthony@kog.com',
+        eyeColor: 'brown', friends: ['Goldie Gentry', 'Briana Decker'], isActive: true, balance: 2764,
+        skills: ['lorem', 'veniam', 'culpa'], gender: 'female', age: 39,
+
+    },
+
+];
+const getUserNames = users => {
+  
+  };
+  
+  console.log(getUserNames(users));
